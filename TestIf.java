26592ad58987bef56ceb1972377690e5be457e4a
paulino-ng-fmu <paulino.ng@fmu.br>
2016-03-28 01:40:22 -0300

    Added files via upload

---
+++ TestIf.java
@@ -0,0 +1,19 @@
+/**
+ * TestIf - esta classe testa diferentes formas de ifs e expressões
+ * 		condicionais.
+ */
+package aula0328;
+
+/**
+ * @author pang2
+ *
+ */
+public class TestIf {
+	public static void main(String[] args) {
+		// i = random;
+		// loop:
+		//   read n
+		//   if n != i goto loop
+		// end_loop
+	}
+}
